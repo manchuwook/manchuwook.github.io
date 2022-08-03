56430f91a8781879da9611020d8e568e695f1e1f
Steven Altsman <manchuwook@tinhalo.com>
2022-08-03 15:36:21 -0500

    Site formatting and update

--- openRoller.js
+++ openRoller.js
@@ -1,4 +1,4 @@
-function openRoller(dice){
+function openRoller(dice) {
 	var diceTower = document.querySelector('#dice-tower');
 	var diceAmount = diceTower.querySelector('#dice');
 	diceAmount.innerHTML = dice;
